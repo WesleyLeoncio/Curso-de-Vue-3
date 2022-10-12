51f5e6020305c902053ed16d4b97382878ba52b7
WESLEY LEONCIO <wesley.leoncio.izi@gmail.com>
2022-10-12 12:08:31 -0300

    criando componente em vue

---
+++ projeto_vue/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <OlaMundo/>
+</template>
+
+<script>
+import OlaMundo from "./components/OlaMundo.vue"
+  export default{
+    name: "app",
+    components:{
+      OlaMundo
+    }
+    
+  }
+</script>
