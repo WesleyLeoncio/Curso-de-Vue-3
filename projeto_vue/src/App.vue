f6f73155a2f76a85082dd651deae52249ef05d63
WESLEY LEONCIO <wesley.leoncio.izi@gmail.com>
2022-10-12 13:32:34 -0300

    LyfeCycle vue

--- projeto_vue/src/App.vue
+++ projeto_vue/src/App.vue
@@ -1,13 +1,19 @@
 <template>
-  <OlaMundo/>
+  <div>
+    <OlaMundo/>
+    <LyfeCycle/>
+  </div>
+  
 </template>
 
 <script>
-import OlaMundo from "./components/OlaMundo.vue"
+import OlaMundo from "./components/OlaMundo.vue";
+import LyfeCycle from "./components/LyfeCycle.vue"
   export default{
     name: "app",
     components:{
-      OlaMundo
+      OlaMundo,
+      LyfeCycle
     }
     
   }
---
+++ projeto_vue/src/components/LyfeCycle.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>Meu nome é: {{nome}}</h1>
+</template>
+
+<script>
+export default{
+    name: "LyfeCycle",
+    data(){
+        return {
+            nome: "NOME DE TESTE"
+        }
+    }, 
+    created(){
+        setTimeout(()=>{
+            this.nome = "Ahri Nova"
+        },1000);
+        
+    },
+    mounted(){
+        setTimeout(()=>{
+            this.nome = "Fizz infernal"
+        },2000);
+    }
+}
+</script>
