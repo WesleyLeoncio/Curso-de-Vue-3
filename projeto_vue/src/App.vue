dd3211190c41e1667070dc4e06bc2e8704a44b63
WESLEY LEONCIO <wesley.leoncio.izi@gmail.com>
2022-10-12 21:35:52 -0300

    Hierarquia de componentes

--- projeto_vue/src/App.vue
+++ projeto_vue/src/App.vue
@@ -2,18 +2,21 @@
   <div>
     <OlaMundo/>
     <LyfeCycle/>
+    <Formulario/>
   </div>
   
 </template>
 
 <script>
 import OlaMundo from "./components/OlaMundo.vue";
-import LyfeCycle from "./components/LyfeCycle.vue"
+import LyfeCycle from "./components/LyfeCycle.vue";
+import Formulario from "./components/form/Formulario.vue";
   export default{
     name: "app",
     components:{
       OlaMundo,
-      LyfeCycle
+      LyfeCycle,
+      Formulario
     }
     
   }
---
+++ projeto_vue/src/components/form/Formulario.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <div>
+      <ImputText/>
+    </div>
+    <div>
+      <ImputText/>
+    </div>
+    <div>
+      <Submit/>
+    </div>
+     
+</div>
+
+</template>
+
+<script>
+import ImputText from "./ImputText.vue"
+import Submit from "./Submit.vue"
+export default{
+    name: "Formulario",
+    components:{
+       ImputText,
+       Submit 
+    }
+}
+</script>
