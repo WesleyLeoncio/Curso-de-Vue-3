daaba9a76ac23ad335157c8f815c3205f3d899ba
WESLEY LEONCIO <wesley.leoncio.izi@gmail.com>
2022-10-15 00:21:57 -0300

    Metodos em vue

--- projeto_vue/src/components/usuario/Info.vue
+++ projeto_vue/src/components/usuario/Info.vue
@@ -6,7 +6,10 @@
     <div>
       <p v-if="trabalhando_bool">Estou Trabalhando!</p>
       <p v-else>Estou desempregado!</p>
-      <p v-show="email_bool">Meu email é: {{ email }}</p>
+      <div>
+        <button @click="showEmail">{{ textBotton }}</button>
+      </div>
+      <p v-show="email_bool">E-mail: {{ email }}</p>
     </div>
   </div>
 </template>
@@ -22,8 +25,19 @@ export default {
     return {
       trabalhando_bool: false,
       email: "email@gmail.com",
-      email_bool: true,
+      email_bool: false,
+      textBotton: "Mostrar e-mail",
     };
+  },
+  methods: {
+    showEmail() {
+      this.email_bool = !this.email_bool;
+      if (this.email_bool) {
+        this.textBotton = "Esconder e-mail";
+      } else {
+        this.textBotton = "Mostrar e-mail";
+      }
+    },
   },
 };
 </script>
