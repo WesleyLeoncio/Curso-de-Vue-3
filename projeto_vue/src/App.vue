056dde0abebb47070812e26f946547b7d338a852
WESLEY LEONCIO <wesley.leoncio.izi@gmail.com>
2022-10-15 22:56:21 -0300

    CSS

--- projeto_vue/src/App.vue
+++ projeto_vue/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Header />
     <OlaMundo />
     <LyfeCycle />
     <Formulario />
@@ -7,15 +8,24 @@
 </template>
 
 <script>
+import Header from "./components/Header.vue";
 import OlaMundo from "./components/OlaMundo.vue";
 import LyfeCycle from "./components/LyfeCycle.vue";
 import Formulario from "./components/form/Formulario.vue";
 export default {
   name: "app",
   components: {
+    Header,
     OlaMundo,
     LyfeCycle,
     Formulario,
   },
 };
 </script>
+
+<style>
+body {
+  background-color: #333;
+  color: #fff;
+}
+</style>
