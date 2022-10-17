be1f30e929c2bc342866f2766139244f9a97d07c
WESLEY LEONCIO <wesley.leoncio.izi@gmail.com>
2022-10-17 13:55:23 -0300

    Passando parametros com props vue

--- projeto_vue/src/components/OlaMundo.vue
+++ projeto_vue/src/components/OlaMundo.vue
@@ -1,25 +1,30 @@
 <template>
   <div>
     <MultiplosEventos />
+    <Reutilizacao />
+    <Reutilizacao />
     <h1>{{ msg }}</h1>
     <h1>{{ nome }}</h1>
-    <info />
+    <info :user="user"/>
   </div>
 </template>
 
 <script>
 import Info from "./usuario/Info.vue";
+import Reutilizacao from "./Reutilizacao.vue";
 import MultiplosEventos from "./MultplosEventos.vue";
 export default {
   name: "OlaMundo",
   components: {
-    MultiplosEventos ,
+    MultiplosEventos,
     Info,
+    Reutilizacao,
   },
   data() {
     return {
       nome: "Nome: Wesley Leôncio Da Cruz",
       msg: "Mensagem: Ola Mundo!",
+      user: {email:"wesley@gmail.com", trabalhando_bool: false}
     };
   },
 };
--- projeto_vue/src/components/usuario/Info.vue
+++ projeto_vue/src/components/usuario/Info.vue
@@ -4,7 +4,7 @@
       <Avatar />
     </div>
     <div>
-      <p v-if="trabalhando_bool">Estou Trabalhando!</p>
+      <p v-if="user.trabalhando_bool">Estou Trabalhando!</p>
       <p v-else>Estou desempregado!</p>
       <p>Utilizo as seguintes tecnoloogias para back-end:</p>
       <ul>
@@ -21,7 +21,7 @@
       <div>
         <button @click="showEmail">{{ textBotton }}</button>
       </div>
-      <p v-show="email_bool">E-mail: {{ email }}</p>
+      <p v-show="email_bool">E-mail: {{ user.email }}</p>
     </div>
   </div>
 </template>
@@ -33,10 +33,11 @@ export default {
   components: {
     Avatar,
   },
+  props: {
+    user: Object,
+  },
   data() {
     return {
-      trabalhando_bool: false,
-      email: "email@gmail.com",
       email_bool: false,
       textBotton: "Mostrar e-mail",
       backend_tech: ["Java", "Node", "C#"],
