941e4ca7e528a683e77271daa1c65abd64f93ba9
WESLEY LEONCIO <wesley.leoncio.izi@gmail.com>
2022-10-17 18:15:44 -0300

    emits

--- projeto_vue/src/components/usuario/Avatar.vue
+++ projeto_vue/src/components/usuario/Avatar.vue
@@ -1,15 +1,27 @@
 <template>
-  <img :src="avatar" :alt="descricao" />
+  <div>
+    <img :src="avatar" :alt="descricao" />
+    <MudarImagem @mudar-imagem="trocarImagem" />
+  </div>
 </template>
 
 <script>
+import MudarImagem from "./MudarImagem.vue";
 export default {
   name: "Avatar",
+  components: {
+    MudarImagem,
+  },
   data() {
     return {
       avatar: "/img/avatar.png",
       descricao: "Wesley Leôncio Da Cruz",
     };
+  },
+  methods: {
+    trocarImagem() {
+      this.avatar = "/img/avatar2.png";
+    },
   },
 };
 </script>
---
+++ projeto_vue/src/components/usuario/MudarImagem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button @click="$emit('mudarImagem')">Mudar Imagem</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MudarImagem",
+  emits: ["mudarImagem"],
+};
+</script>
+
+<style>
+</style>
