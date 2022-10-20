55de0f865f029135091b38a0b6c546845a4b2256
WESLEY LEONCIO <wesley.leoncio.izi@gmail.com>
2022-10-20 00:40:04 -0300

    Criação do formulario

--- projeto_final/src/views/Home.vue
+++ projeto_final/src/views/Home.vue
@@ -3,14 +3,16 @@
     <Banner />
     <div class="main-container">
       <h1>Monte o seu buger:</h1>
+      <BurgerForm />
     </div>
   </div>
 </template>
 
 <script>
 import Banner from "../components/Banner.vue";
+import BurgerForm from "../components/BurgerForm.vue";
 export default {
-  components: { Banner },
+  components: { Banner, BurgerForm },
   name: "Home",
 };
 </script>
